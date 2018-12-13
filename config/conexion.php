5ab03d31591dc4b35b6f352df0bb75c7e584e00e
Jano <alejanoo.f@gmail.com>
2018-12-13 00:39:07 -0300

    Añadir Archivos

---
+++ config/conexion.php
@@ -0,0 +1,34 @@
+<?php
+requiere_once "global.php";
+
+$conexion = new Mysqli(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD);
+
+mysqli_query($conexion, 'SET NAME "'.DB_ENCODE.'"');
+
+if(mysqli_connect_errno())
+{
+    printgf("Conexión fallida: %s\n",mysqli_connect_error());
+    exit();
+}
+
+if(!function_exits("ejecutarConsulta"))
+{
+    function ejecutarConsulta($sql)
+    {
+        global $conexion;
+        $query = $conexion->query($sql);
+        return $query;
+    }
+
+    function ejecutarConsultaSimple($sql)
+    {
+        global $conexion;
+        $query = $conexion->query($sql);
+        $row = $query->fetch_assoc();
+        return $row;
+    }
+
+    function ejecutarConsulta
+}
+
+?>
---
+++ config/global.php
@@ -0,0 +1,8 @@
+<?php
+define("DB_HOST","localhost");
+define("DB_NAME","MAsistencia");
+define("DB_USERNAME","root");
+define("DB_PASSWORD","");
+define("DB_ENCODE","utf8");
+define("PRO_NOMBRE", "MARCAR ASISTENCIA");
+?>
